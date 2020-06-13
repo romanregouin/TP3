6324236d0bb2ab219e8655a0d47b2425007536fe
Liam Andrieux <andrieuxliam@gmail.com>
2020-06-13 21:24:46 +0200

    des modifs

--- source_graphe/test_graphe.c
+++ source_graphe/test_graphe.c
@@ -3,7 +3,7 @@
 
 #include "graphe.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   pgraphe_t g;
   // int nc;
 
@@ -50,18 +50,43 @@ int main(int argc, char **argv) {
 
   if (complet(g)) printf("Le graphe est Complet ! \n");
 
-  int* labels = malloc(4*sizeof(int));
-  labels[0] = 0; labels [1] = 2; labels[2] = 8; labels[3] = 5;
+  int* labels = malloc(6 * sizeof(int));
+  /*labels[0] = 0;
+  labels[1] = 2;
+  labels[2] = 8;
+  labels[3] = 5;
 
-  pchemin_t chemin = creerChemin(g,labels,4);
+  pchemin_t chemin = creerChemin(g, labels, 4);
+
   printChemin(chemin);
-  if(elementaire(chemin)){
+  printf("Test Roman \n\n");
+  if (elementaire(chemin)) {
     printf("Le chemin est élémentaire\n");
   }
-  if(simple(chemin)){
+  if (simple(chemin)) {
     printf("Le chemin est simple\n");
   }
-  printf("La distance entre %d et %d est de %d \n", labels[0], labels[1], distance(g,labels[0],labels[1]));
+*/
+  labels[0] = 1;
+  labels[1] = 2;
+  labels[2] = 4;
+  labels[3] = 1;
+  labels[4] = 3;
+  labels[5] = 4;
+  labels[6] = 1;
+
+  pchemin_t chemin = creerChemin(g, labels, 6);
+  printf("Test Liam \n\n");
+  printChemin(chemin);
+  if (elementaire(chemin)) {
+    printf("Le chemin est élémentaire\n");
+  }
+  if (simple(chemin)) {
+    printf("Le chemin est simple\n");
+  }
+
+  printf("La distance entre %d et %d est de %d \n", labels[0], labels[1],
+         distance(g, labels[0], labels[1]));
   printf("Degre entrant du sommet %d : %d \n", label,
          degre_entrant_sommet(g, chercher_sommet(g, label)));
   printf("Degre sortant du sommet %d : %d \n", label,
